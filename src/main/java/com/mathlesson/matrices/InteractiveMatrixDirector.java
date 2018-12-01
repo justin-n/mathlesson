960c1e31eb1dfc06601731a8b85855e3a6e8a4d2
Justin <justin.n.4rws@gmail.com>
2018-11-30 21:38:43 -0500

    Handled exceptions in InteractiveMatrixDirector

--- src/main/java/com/mathlesson/matrices/InteractiveMatrixDirector.java
+++ src/main/java/com/mathlesson/matrices/InteractiveMatrixDirector.java
@@ -13,54 +13,71 @@ public class InteractiveMatrixDirector {
 
     private static final String CURSOR = ">> ";
 
-    public static void runSimpleMenuInterpreter() throws IOException {
+    public static void runSimpleMenuInterpreter() {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = getSystemInputBufferedReader();
 
         BinaryMatrixOperationEquationHandler
             bmoeh = new BinaryMatrixOperationEquationHandler();
 
         int mainMenuChoice = 0;
 
-        while (mainMenuChoice != 6) {
-            System.out.println();
-            System.out.println("\t1. Add row to first matrix in operation order");
-            System.out.println("\t2. Add row to second matrix in operation order");
-            System.out.println("\t3. View first matrix in operation order");
-            System.out.println("\t4. View second matrix in operation order");
-            System.out.println("\t5. View both matrix operands");
-            System.out.println("\t6. Exit");
-            System.out.println();
-            System.out.print(CURSOR);
-
-            String mainMenuChoiceLine = br.readLine();
-
-            while (!InputValidationUtil.validateMenuChoiceInput(mainMenuChoiceLine)) {
+        try {
+            while (mainMenuChoice != 6) {
+                System.out.println();
+                System.out.println("\t1. Add row to first matrix in operation order");
+                System.out.println("\t2. Add row to second matrix in operation order");
+                System.out.println("\t3. View first matrix in operation order");
+                System.out.println("\t4. View second matrix in operation order");
+                System.out.println("\t5. View both matrix operands");
+                System.out.println("\t6. Exit");
                 System.out.println();
                 System.out.print(CURSOR);
-                mainMenuChoiceLine = br.readLine();
-            }
 
-            mainMenuChoice = Integer.parseInt(mainMenuChoiceLine);
+                String mainMenuChoiceLine = br.readLine();
 
-            if (mainMenuChoice == 1) {
-                addRowToMatrix(br, bmoeh.getFirstMatrix());
+                while (!InputValidationUtil.validateMenuChoiceInput(mainMenuChoiceLine)) {
+                    System.out.println();
+                    System.out.print(CURSOR);
+                    mainMenuChoiceLine = br.readLine();
+                }
+
+                mainMenuChoice = Integer.parseInt(mainMenuChoiceLine);
+
+                if (mainMenuChoice == 1) {
+                    addRowToMatrix(br, bmoeh.getFirstMatrix());
+                }
+                else if (mainMenuChoice == 2) {
+                    addRowToMatrix(br, bmoeh.getSecondMatrix());
+                }
+                else if (mainMenuChoice == 3) {
+                    System.out.println("First matrix in operation order: ");
+                    System.out.println(bmoeh.getFirstMatrix());
+                }
+                else if (mainMenuChoice == 4) {
+                    System.out.println("Second matrix in operation order: ");
+                    System.out.println(bmoeh.getSecondMatrix());
+                }
+                else if (mainMenuChoice == 5) {
+                    bmoeh.printOperands();
+                }
             }
-            else if (mainMenuChoice == 2) {
-                addRowToMatrix(br, bmoeh.getSecondMatrix());
-            }
-            else if (mainMenuChoice == 3) {
-                System.out.println("First matrix in operation order: ");
-                System.out.println(bmoeh.getFirstMatrix());
-            }
-            else if (mainMenuChoice == 4) {
-                System.out.println("Second matrix in operation order: ");
-                System.out.println(bmoeh.getSecondMatrix());
-            }
-            else if (mainMenuChoice == 5) {
-                bmoeh.printOperands();
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    private static BufferedReader getSystemInputBufferedReader() {
+
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return br;
     }
 
     private static void addRowToMatrix(BufferedReader br, Matrix matrix) throws IOException {
